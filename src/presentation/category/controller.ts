d9bfa8a797af0026c20ea3651fd4884c53f96558
pausm99 <pausm99@gmail.com>
2025-09-08 13:03:06 +0200

    create category routes and controller

---
+++ src/presentation/category/controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express'
+import { CustomError } from '../../domain'
+
+export class CategoryController {
+	constructor() {}
+
+	private handleError = (error: unknown, res: Response) => {
+		if (error instanceof CustomError) {
+			return res.status(error.statusCode).json({ error: error.message })
+		}
+		console.log(error)
+		return res.status(500).json({ error: 'Internal server error' })
+	}
+
+	createCategory = async (req: Request, res: Response) => {
+        res.json('Create Category')
+    }
+
+    getCategories = async (req: Request, res: Response) => {
+        res.json('Get Categories')
+    }
+}
